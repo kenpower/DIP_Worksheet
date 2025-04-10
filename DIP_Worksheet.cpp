777618face09fa71876bbb96e4610a597e9a878d
Ken Power <ken@kenpower.com>
2025-04-10 09:58:51 +0100

    run takes a reference to a DrawableShape interface and nolonger has a dependency on sf::CircleShape

--- DIP_Worksheet.cpp
+++ DIP_Worksheet.cpp
@@ -21,41 +21,104 @@
 #include <iostream>
 #include <Windows.h>
 
-class Game {
-    int screenWidth = 1200;
-    int screenHeight = 800;
+#include <SFML/Graphics.hpp>
+
+class DrawableShape {
 public:
-    void run() {
-		{
-			sf::RenderWindow window(sf::VideoMode(screenWidth, screenHeight), "SFML Application");
-			window.setFramerateLimit(25);
-			sf::CircleShape shape(100.f);
-			shape.setFillColor(sf::Color::Green);
-			while (window.isOpen())
-			{
-				sf::Event event;
-				while (window.pollEvent(event))
-				{
-					if (event.type == sf::Event::Closed)
-						window.close();
-				}
-				
-				shape.setPosition(rand() % screenWidth, rand() % screenHeight);
-				shape.setFillColor(sf::Color(rand() % 255, rand() % 255, rand() % 255));
-				
-				window.draw(shape);
-				window.display();
-				window.draw(shape); //paint on second buffer also to avoid flicker
-			}
-		}
+    virtual void draw(sf::RenderWindow& window) = 0;
+};
+
+class CircleShape : public DrawableShape {
+private:
+    sf::CircleShape shape;
+
+public:
+    CircleShape(float radius) : shape(radius) {}
+
+    void setup() {
+		shape.setFillColor(sf::Color::Green);
+        shape.setRadius(100.f);
+	}
+    void draw(sf::RenderWindow& window) override {
+        window.draw(shape);
     }
 };
 
+class Game {
+    int screenWidth = 1200;
+    int screenHeight = 800;
+
+public:
+
+    // IMPORTANT: run takes a reference to a DrawableShape interface and nolonger has a dependency on sf::CircleShape
+    void run(DrawableShape& shape) {
+        sf::RenderWindow window(sf::VideoMode(screenWidth, screenHeight), "SFML Application");
+        window.setFramerateLimit(25);
+
+        while (window.isOpen()) {
+            sf::Event event;
+            while (window.pollEvent(event)) {
+                if (event.type == sf::Event::Closed)
+                    window.close();
+            }
+
+            shape.draw(window);
+            window.display();
+
+        }
+    }
+};
+
+
 int main() {
+    CircleShape circleShape(100.f);
+    circleShape.setup();
 
-	Game game;
+    Game game; // Pass the CircleShape instance to the Game constructor
+    game.run(circleShape);
 
-	game.run();
-	
     return 0;
 }
+
+
+
+// Original ====================================================================================
+//
+//class Game {
+//    int screenWidth = 1200;
+//    int screenHeight = 800;
+//public:
+//    void run() {
+//		{
+//			sf::RenderWindow window(sf::VideoMode(screenWidth, screenHeight), "SFML Application");
+//			window.setFramerateLimit(25);
+//			sf::CircleShape shape(100.f);
+//			shape.setFillColor(sf::Color::Green);
+//			while (window.isOpen())
+//			{
+//				sf::Event event;
+//				while (window.pollEvent(event))
+//				{
+//					if (event.type == sf::Event::Closed)
+//						window.close();
+//				}
+//				
+//				shape.setPosition(rand() % screenWidth, rand() % screenHeight);
+//				shape.setFillColor(sf::Color(rand() % 255, rand() % 255, rand() % 255));
+//				
+//				window.draw(shape);
+//				window.display();
+//				window.draw(shape); //paint on second buffer also to avoid flicker
+//			}
+//		}
+//    }
+//};
+//
+//int main() {
+//
+//	Game game;
+//
+//	game.run();
+//	
+//    return 0;
+//}
