099a5146d546d60e0e83c050cdbafd768edaffaa
Ken Power <github@kenpower.com>
2026-02-25 14:10:39 +0000

    Update DIP_Worksheet.cpp

--- DIP_Worksheet.cpp
+++ DIP_Worksheet.cpp
@@ -1,4 +1,4 @@
-﻿#ifdef _DEBUG 
+#ifdef _DEBUG 
 #pragma comment(lib,"sfml-graphics-d.lib") 
 #pragma comment(lib,"sfml-audio-d.lib") 
 #pragma comment(lib,"sfml-system-d.lib") 
@@ -21,33 +21,43 @@
 #include <iostream>
 #include <Windows.h>
 
-class Game {
-    int screenWidth = 1200;
-    int screenHeight = 800;
+#include <SFML/Graphics.hpp>
+#include <cstdlib>
+
+class Game
+{
+    std::uint32_t screenWidth = 1200;
+    std::uint32_t screenHeight = 800;
+
 public:
-    void run() {
-		{
-			sf::RenderWindow window(sf::VideoMode(screenWidth, screenHeight), "SFML Application");
-			window.setFramerateLimit(25);
-			sf::CircleShape shape(100.f); // TODO: DIP Violation - Game (high-level) depends directly on sf::CircleShape (low-level)
-			shape.setFillColor(sf::Color::Green);
-			while (window.isOpen())
-			{
-				sf::Event event;
-				while (window.pollEvent(event))
-				{
-					if (event.type == sf::Event::Closed)
-						window.close();
-				}
-				
-				shape.setPosition(rand() % screenWidth, rand() % screenHeight);
-				shape.setFillColor(sf::Color(rand() % 255, rand() % 255, rand() % 255));
-				
-				window.clear();
-				window.draw(shape);
-				window.display();
-			}
-		}
+    void run()
+    {
+        sf::RenderWindow window(
+            sf::VideoMode({ screenWidth, screenHeight }), "SFML Application");
+        //window.setFrameRateLimit(25);
+
+        sf::CircleShape shape(100.f);   // TODO: DIP violation still present
+        shape.setFillColor(sf::Color::Green);
+
+        while (window.isOpen())
+        {
+            while (const std::optional event = window.pollEvent())
+            {
+                if (event->is<sf::Event::Closed>())
+                    window.close();
+            }
+
+            shape.setPosition({ static_cast<float>(std::rand() % screenWidth),
+                static_cast<float>(std::rand() % screenHeight) });
+            shape.setFillColor(
+                sf::Color{ static_cast<std::uint8_t>(std::rand() % 256),
+                          static_cast<std::uint8_t>(std::rand() % 256),
+                          static_cast<std::uint8_t>(std::rand() % 256) });
+
+            window.clear();
+            window.draw(shape);
+            window.display();
+        }
     }
 };
 
